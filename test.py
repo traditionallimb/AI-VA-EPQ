371f43096b5c34f60318b683426fcd71ec3393a2
traditionallimb <dom@kalowe.me.uk>
2022-12-07 12:26:55 +0000

    Change the API used for SpeechRecognition

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 import speech_recognition as sr
+import os
+
+CLIENTID="zqycSsvgNb1Bb4ZJgkubxw=="
+CLIENTKEY="p7bbJOrZDr232RrjGxUkOzNx9pHphVDcsR6eeTw6Zp96yNYuFesaWTpcdc5pOY_3sST6dWroLak2vB1vmQ804w=="
 
 r = sr.Recognizer()
 
@@ -6,4 +10,4 @@ harvard = sr.AudioFile('audio_files_harvard.wav')
 with harvard as source:
     audio = r.record(source)
 
-print(r.recognize_google(audio))
+print(r.recognize_houndify(audio, client_id=CLIENTID, client_key=CLIENTKEY))
