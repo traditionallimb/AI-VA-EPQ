e1c86a617871d3ffb60d41b1d902c85d049d4b35
traditionallimb <dom@kalowe.me.uk>
2022-12-08 16:46:05 +0000

    Change some more things

--- test.py
+++ test.py
@@ -10,4 +10,6 @@ harvard = sr.AudioFile('audio_files_harvard.wav')
 with harvard as source:
     audio = r.record(source)
 
-print(r.recognize_houndify(audio, client_id=CLIENTID, client_key=CLIENTKEY))
+print("START")
+out = r.recognize_google(audio)
+print(out)
