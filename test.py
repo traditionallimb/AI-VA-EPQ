1b1cee6fdeaea88ab19f2bda7ab486697dfe53b5
traditionallimb <dom@kalowe.me.uk>
2022-12-10 16:21:34 +0000

    I DID IT

--- test.py
+++ test.py
@@ -4,12 +4,32 @@ import os
 CLIENTID="zqycSsvgNb1Bb4ZJgkubxw=="
 CLIENTKEY="p7bbJOrZDr232RrjGxUkOzNx9pHphVDcsR6eeTw6Zp96yNYuFesaWTpcdc5pOY_3sST6dWroLak2vB1vmQ804w=="
 
-r = sr.Recognizer()
+# r = sr.Recognizer()
 
-harvard = sr.AudioFile('audio_files_harvard.wav')
-with harvard as source:
-    audio = r.record(source)
+# harvard = sr.AudioFile('audio_files_harvard.wav')
+# with harvard as source:
+#     audio = r.record(source)
 
-print("START")
-out = r.recognize_google(audio)
-print(out)
+# print(type(audio))
+# print(type(source))
+
+# print("START")
+# out = r.recognize_google(audio)
+# print(out)
+# print(type(out))
+
+
+def wavToText(filePath):
+    r = sr.Recognizer()
+
+    harvard = sr.AudioFile(filePath)
+    with harvard as source:
+        audio = r.record(source)
+    try:
+        return r.recognize_houndify(audio, client_id=CLIENTID, client_key=CLIENTKEY, show_all=True)
+    except UnknownValueError:
+        raise GolemException("Could not understand audio")
+
+out=wavToText('audio_files_harvard.wav')
+
+print(out['AllResults'][0]['WrittenResponse'])
