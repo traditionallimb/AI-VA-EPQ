4557871483076a6b71acdcaf32cded325364c669
traditionallimb <dom@kalowe.me.uk>
2022-12-16 09:13:01 +0000

    Added error handling to the test, and almost finished the microphone test file

--- microphone.py
+++ microphone.py
@@ -1,22 +1,25 @@
 import speech_recognition as sr
-import os
 
 CLIENTID="zqycSsvgNb1Bb4ZJgkubxw=="
 CLIENTKEY="p7bbJOrZDr232RrjGxUkOzNx9pHphVDcsR6eeTw6Zp96yNYuFesaWTpcdc5pOY_3sST6dWroLak2vB1vmQ804w=="
 
-def wavToText(filePath):
-    r = sr.Recognizer()
 
-    harvard = sr.AudioFile(filePath)
-    with harvard as source:
-        audio = r.record(source)
+
+sr.Microphone()
+
+def mic_to_text():
+    r = sr.Recognizer()
+    mic = sr.Microphone()
+
+    with mic as source:
+        audio = r.listen(source)
     try:
         return r.recognize_houndify(audio, client_id=CLIENTID, client_key=CLIENTKEY, show_all=True)
     except UnknownValueError:
         raise GolemException("Could not understand audio")
 
-out = wavToText('audio_files_harvard.wav')
+out = mic_to_text()
 
-response = out['AllResults'][0]['WrittenResponse']
+response = out['AllResults'][0]["WrittenResponse"]
 
 print(response)
