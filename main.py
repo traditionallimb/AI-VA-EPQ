160b6e0c3ee446451e73cf2b30e732c8c2e7bb10
traditionallimb <dom@kalowe.me.uk>
2023-02-15 22:45:34 +0000

    new stuff???

---
+++ main.py
@@ -0,0 +1,6 @@
+import micClass as mc
+
+inp = mc.Mic()
+
+print(inp.micToText("zqycSsvgNb1Bb4ZJgkubxw==", "p7bbJOrZDr232RrjGxUkOzNx9pHphVDcsR6eeTw6Zp96yNYuFesaWTpcdc5pOY_3sST6dWroLak2vB1vmQ804w=="))
+print("Finished")
--- micClass.py
+++ micClass.py
@@ -1,1 +1,16 @@
 #! THIS IS A TEMPORARY FILE JUST AS A PLACEHOLDER TO REMIND MYSELF TO MAKE THE MIC CLASS!
+import speech_recognition as sr
+
+class Mic:
+
+    def micToText(self, clientID, clientKey):
+        with sr.Microphone() as source:
+            audio = sr.Recognizer().listen(source)
+        try:
+            out = sr.Recognizer().recognize_houndify(audio, client_id=clientID, client_key=clientKey, show_all=True)
+        except UnknownValueError:
+            raise GolemException("Could not understand audio")
+        else:
+            print("AHGAHGAHGAHGAHGAHGAHGAHGAHG") # this line should never be executed
+
+        return out['AllResults'][0]["WrittenResponse"]
